f4d7ff270256017762a8c01aa8983e29181d3372
Jake Barnette <jb172617@ohio.edu>
2021-05-14 12:58:54 -0400

    Inital Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jake Barnette
+ *        Date: 5/14/2021
+ * Description: This is a mock program for the git hw assignment
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!"<< endl;
+    return 0;
+}// main
